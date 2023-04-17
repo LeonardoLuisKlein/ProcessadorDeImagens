428c07a2b88d917343007e78f9596683ca9a4860
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2023-04-16 22:32:36 -0300

    Feat: exercise 7 and 9 completed

--- processamento-de-imagens/Form1.Designer.cs
+++ processamento-de-imagens/Form1.Designer.cs
@@ -36,6 +36,9 @@ namespace processamento_de_imagens
             this.btCarregaImgB = new System.Windows.Forms.Button();
             this.imgB = new System.Windows.Forms.PictureBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.nupBlending = new System.Windows.Forms.NumericUpDown();
+            this.nupMultiplicacao = new System.Windows.Forms.NumericUpDown();
+            this.nupDivisao = new System.Windows.Forms.NumericUpDown();
             this.btBlending = new System.Windows.Forms.Button();
             this.btMedia = new System.Windows.Forms.Button();
             this.btDivisao = new System.Windows.Forms.Button();
@@ -50,20 +53,29 @@ namespace processamento_de_imagens
             this.btXor = new System.Windows.Forms.Button();
             this.btOr = new System.Windows.Forms.Button();
             this.btAnd = new System.Windows.Forms.Button();
-            this.nupDivisao = new System.Windows.Forms.NumericUpDown();
-            this.nupMultiplicacao = new System.Windows.Forms.NumericUpDown();
-            this.nupBlending = new System.Windows.Forms.NumericUpDown();
+            this.btExer9 = new System.Windows.Forms.Button();
+            this.btExer7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.imgA)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.imgB)).BeginInit();
             this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nupBlending)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupMultiplicacao)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupDivisao)).BeginInit();
             this.groupBox4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.imgFinal)).BeginInit();
             this.groupBox5.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.nupDivisao)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nupMultiplicacao)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nupBlending)).BeginInit();
             this.SuspendLayout();
             // 
             // imgA
@@ -143,6 +155,30 @@ namespace processamento_de_imagens
             this.groupBox3.TabIndex = 3;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Operações aritméticas";
+            // 
+            // nupBlending
+            // 
+            this.nupBlending.DecimalPlaces = 2;
+            this.nupBlending.Location = new System.Drawing.Point(125, 184);
+            this.nupBlending.Name = "nupBlending";
+            this.nupBlending.Size = new System.Drawing.Size(75, 20);
+            this.nupBlending.TabIndex = 10;
+            // 
+            // nupMultiplicacao
+            // 
+            this.nupMultiplicacao.DecimalPlaces = 2;
+            this.nupMultiplicacao.Location = new System.Drawing.Point(125, 88);
+            this.nupMultiplicacao.Name = "nupMultiplicacao";
+            this.nupMultiplicacao.Size = new System.Drawing.Size(75, 20);
+            this.nupMultiplicacao.TabIndex = 9;
+            // 
+            // nupDivisao
+            // 
+            this.nupDivisao.DecimalPlaces = 2;
+            this.nupDivisao.Location = new System.Drawing.Point(125, 120);
+            this.nupDivisao.Name = "nupDivisao";
+            this.nupDivisao.Size = new System.Drawing.Size(75, 20);
+            this.nupDivisao.TabIndex = 8;
             // 
             // btBlending
             // 
@@ -288,35 +324,138 @@ namespace processamento_de_imagens
             this.btAnd.UseVisualStyleBackColor = true;
             this.btAnd.Click += new System.EventHandler(this.btAnd_Click);
             // 
-            // nupDivisao
+            // btExer9
             // 
-            this.nupDivisao.DecimalPlaces = 2;
-            this.nupDivisao.Location = new System.Drawing.Point(125, 120);
-            this.nupDivisao.Name = "nupDivisao";
-            this.nupDivisao.Size = new System.Drawing.Size(75, 20);
-            this.nupDivisao.TabIndex = 8;
+            this.btExer9.Location = new System.Drawing.Point(35, 546);
+            this.btExer9.Name = "btExer9";
+            this.btExer9.Size = new System.Drawing.Size(121, 38);
+            this.btExer9.TabIndex = 5;
+            this.btExer9.Text = "Exer 9";
+            this.btExer9.UseVisualStyleBackColor = true;
+            this.btExer9.Click += new System.EventHandler(this.btExer9_Click);
             // 
-            // nupMultiplicacao
+            // btExer7
             // 
-            this.nupMultiplicacao.DecimalPlaces = 2;
-            this.nupMultiplicacao.Location = new System.Drawing.Point(125, 88);
-            this.nupMultiplicacao.Name = "nupMultiplicacao";
-            this.nupMultiplicacao.Size = new System.Drawing.Size(75, 20);
-            this.nupMultiplicacao.TabIndex = 9;
+            this.btExer7.Location = new System.Drawing.Point(462, 546);
+            this.btExer7.Name = "btExer7";
+            this.btExer7.Size = new System.Drawing.Size(121, 38);
+            this.btExer7.TabIndex = 6;
+            this.btExer7.Text = "Exer 7";
+            this.btExer7.UseVisualStyleBackColor = true;
+            this.btExer7.Click += new System.EventHandler(this.btExer7_Click);
             // 
-            // nupBlending
+            // label1
             // 
-            this.nupBlending.DecimalPlaces = 2;
-            this.nupBlending.Location = new System.Drawing.Point(125, 184);
-            this.nupBlending.Name = "nupBlending";
-            this.nupBlending.Size = new System.Drawing.Size(75, 20);
-            this.nupBlending.TabIndex = 10;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(458, 649);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Matriz 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(626, 649);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Matriz 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(805, 649);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Matriz 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(972, 649);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Matriz 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1129, 649);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Matriz 5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(462, 615);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Matriz 1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(627, 615);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Matriz 2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(812, 615);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Matriz 3";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(973, 615);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Matriz 4";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(1130, 615);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 13);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Matriz 5";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1556, 644);
+            this.ClientSize = new System.Drawing.Size(1556, 807);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btExer7);
+            this.Controls.Add(this.btExer9);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
@@ -329,13 +468,14 @@ namespace processamento_de_imagens
             this.groupBox2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.imgB)).EndInit();
             this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nupBlending)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupMultiplicacao)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupDivisao)).EndInit();
             this.groupBox4.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.imgFinal)).EndInit();
             this.groupBox5.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.nupDivisao)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nupMultiplicacao)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nupBlending)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -365,6 +505,18 @@ namespace processamento_de_imagens
         private System.Windows.Forms.NumericUpDown nupDivisao;
         private System.Windows.Forms.NumericUpDown nupBlending;
         private System.Windows.Forms.NumericUpDown nupMultiplicacao;
+        private System.Windows.Forms.Button btExer9;
+        private System.Windows.Forms.Button btExer7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
 
--- processamento-de-imagens/Form1.cs
+++ processamento-de-imagens/Form1.cs
@@ -36,7 +36,6 @@ namespace processamento_de_imagens
                 MessageBox.Show("Selecione uma imagem valida");
                 return;
             }
-
         }
 
         private void btCarregaImgB_Click(object sender, EventArgs e)
@@ -50,6 +49,11 @@ namespace processamento_de_imagens
             if (result == DialogResult.OK)
             {
                 imgB.Load(openFileDialog.FileName);
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma imagem valida");
+                return;
             }
         }
 
@@ -77,6 +81,9 @@ namespace processamento_de_imagens
                     case 2:
                         format = ImageFormat.Png;
                         break;
+                    case 3:
+                        format = ImageFormat.Tiff;
+                        break;
                     default:
                         format = ImageFormat.Jpeg;
                         break;
@@ -91,7 +98,13 @@ namespace processamento_de_imagens
             Image image1 = imgA.Image;
             Image image2 = imgB.Image;
 
-           if (image1.Width != image2.Width || image1.Height != image2.Height || image1.PixelFormat != image2.PixelFormat)
+            if (image1 == null || image2 == null)
+            {
+                MessageBox.Show("Por favor, selecione duas imagens");
+                return;
+            }
+
+            if (image1.Width != image2.Width || image1.Height != image2.Height || image1.PixelFormat != image2.PixelFormat)
            {
                MessageBox.Show("As imagens precisam ter o mesmo tamanho e formato para serem somadas.");
                return;
@@ -108,16 +121,15 @@ namespace processamento_de_imagens
                     Color color1 = ((Bitmap)image1).GetPixel(x, y);
                     Color color2 = ((Bitmap)image2).GetPixel(x, y);
 
-                    int red = color1.R + color2.R;
-                    int green = color1.G + color2.G;
-                    int blue = color1.B + color2.B;
+                    int r = color1.R + color2.R;
+                    int g = color1.G + color2.G;
+                    int b = color1.B + color2.B;
 
+                    r = Math.Min(r, 255);
+                    g = Math.Min(g, 255);
+                    b = Math.Min(b, 255);
 
-                    red = Math.Min(red, 255);
-                    green = Math.Min(green, 255);
-                    blue = Math.Min(blue, 255);
-
-                    imagemResultado.SetPixel(x, y, Color.FromArgb(red, green, blue));
+                    imagemResultado.SetPixel(x, y, Color.FromArgb(r, g, b));
                 }
             }
             imgFinal.Image = imagemResultado;
@@ -127,6 +139,12 @@ namespace processamento_de_imagens
         {
             Image image1 = imgA.Image;
             Image image2 = imgB.Image;
+
+            if (image1 == null || image2 == null)
+            {
+                MessageBox.Show("Por favor, selecione duas imagens");
+                return;
+            }
 
             if (image1.Width != image2.Width || image1.Height != image2.Height || image1.PixelFormat != image2.PixelFormat)
             {
@@ -145,6 +163,7 @@ namespace processamento_de_imagens
                     int r = Math.Abs(color1.R - color2.R);
                     int g = Math.Abs(color1.G - color2.G);
                     int b = Math.Abs(color1.B - color2.B);
+
                     imagemResultado.SetPixel(x, y, Color.FromArgb(r, g, b));
                 }
             }
@@ -156,6 +175,12 @@ namespace processamento_de_imagens
         {
             Image image1 = imgA.Image;
             Image image2 = imgB.Image;
+
+            if (image1 == null || image2 == null)
+            {
+                MessageBox.Show("Por favor, selecione duas imagens");
+                return;
+            }
 
             if (image1.Width != image2.Width || image1.Height != image2.Height || image1.PixelFormat != image2.PixelFormat)
             {
@@ -194,6 +219,12 @@ namespace processamento_de_imagens
             Image image1 = imgA.Image;
             Image image2 = imgB.Image;
 
+            if (image1 == null || image2 == null)
+            {
+                MessageBox.Show("Por favor, selecione duas imagens");
+                return;
+            }
+
             if (image1.Width != image2.Width || image1.Height != image2.Height || image1.PixelFormat != image2.PixelFormat)
             {
                 MessageBox.Show("As imagens precisam ter o mesmo tamanho e formato para serem somadas.");
@@ -231,6 +262,12 @@ namespace processamento_de_imagens
             Image image1 = imgA.Image;
             Image image2 = imgB.Image;
 
+            if (image1 == null || image2 == null)
+            {
+                MessageBox.Show("Por favor, selecione duas imagens");
+                return;
+            }
+
             if (image1.Width != image2.Width || image1.Height != image2.Height || image1.PixelFormat != image2.PixelFormat)
             {
                 MessageBox.Show("As imagens precisam ter o mesmo tamanho e formato para serem somadas.");
@@ -243,11 +280,11 @@ namespace processamento_de_imagens
             {
                 for (int y = 0; y < image1.Height; y++)
                 {
-                    Color cor1 = ((Bitmap)image1).GetPixel(x, y);
-                    Color cor2 = ((Bitmap)image1).GetPixel(x, y);
-                    int mediaR = (cor1.R + cor2.R) / 2;
-                    int mediaG = (cor1.G + cor2.G) / 2;
-                    int mediaB = (cor1.B + cor2.B) / 2;
+                    Color color1 = ((Bitmap)image1).GetPixel(x, y);
+                    Color color2 = ((Bitmap)image1).GetPixel(x, y);
+                    int mediaR = (color1.R + color2.R) / 3;
+                    int mediaG = (color1.G + color2.G) / 3;
+                    int mediaB = (color1.B + color2.B) / 3;
                     Color corResultado = Color.FromArgb(mediaR, mediaG, mediaB);
                     imagemResultado.SetPixel(x, y, corResultado);
                 }
@@ -260,6 +297,12 @@ namespace processamento_de_imagens
         {
             Image image1 = imgA.Image;
             Image image2 = imgB.Image;
+
+            if (image1 == null || image2 == null)
+            {
+                MessageBox.Show("Por favor, selecione duas imagens");
+                return;
+            }
 
             if (image1.Width != image2.Width || image1.Height != image2.Height || image1.PixelFormat != image2.PixelFormat)
             {
@@ -282,9 +325,9 @@ namespace processamento_de_imagens
                 {
                     Color color1 = ((Bitmap)image1).GetPixel(x, y);
                     Color color2 = ((Bitmap)image2).GetPixel(x, y);
-                    int r = (int)Math.Round((1 - blending) * color1.R + blending * color1.R);
-                    int g = (int)Math.Round((1 - blending) * color1.G + blending * color1.G);
-                    int b = (int)Math.Round((1 - blending) * color1.B + blending * color1.B);
+                    int r = (int)Math.Round((1 - blending) * color1.R + blending * color2.R);
+                    int g = (int)Math.Round((1 - blending) * color1.G + blending * color2.G);
+                    int b = (int)Math.Round((1 - blending) * color1.B + blending * color2.B);
 
                     imagemResultado.SetPixel(x, y, Color.FromArgb(r, g, b));
                 }
@@ -297,6 +340,12 @@ namespace processamento_de_imagens
         {
             Image image1 = imgA.Image;
             Image image2 = imgB.Image;
+
+            if (image1 == null || image2 == null)
+            {
+                MessageBox.Show("Por favor, selecione duas imagens");
+                return;
+            }
 
             if (image1.Width != image2.Width || image1.Height != image2.Height || image1.PixelFormat != image2.PixelFormat)
             {
@@ -325,6 +374,12 @@ namespace processamento_de_imagens
             Image image1 = imgA.Image;
             Image image2 = imgB.Image;
 
+            if (image1 == null || image2 == null)
+            {
+                MessageBox.Show("Por favor, selecione duas imagens");
+                return;
+            }
+
             if (image1.Width != image2.Width || image1.Height != image2.Height || image1.PixelFormat != image2.PixelFormat)
             {
                 MessageBox.Show("As imagens precisam ter o mesmo tamanho e formato para serem somadas.");
@@ -351,6 +406,12 @@ namespace processamento_de_imagens
         {
             Image image1 = imgA.Image;
             Image image2 = imgB.Image;
+
+            if (image1 == null || image2 == null)
+            {
+                MessageBox.Show("Por favor, selecione duas imagens");
+                return;
+            }
 
             if (image1.Width != image2.Width || image1.Height != image2.Height || image1.PixelFormat != image2.PixelFormat)
             {
@@ -379,6 +440,12 @@ namespace processamento_de_imagens
             Image image1 = imgA.Image;
             Image image2 = imgB.Image;
 
+            if (image1 == null || image2 == null)
+            {
+                MessageBox.Show("Por favor, selecione duas imagens");
+                return;
+            }
+
             if (image1.Width != image2.Width || image1.Height != image2.Height || image1.PixelFormat != image2.PixelFormat)
             {
                 MessageBox.Show("As imagens precisam ter o mesmo tamanho e formato para serem somadas.");
@@ -403,6 +470,157 @@ namespace processamento_de_imagens
             }
 
             imgFinal.Image = imagemResultado;
+        }
+
+        private void btExer9_Click(object sender, EventArgs e)
+        {
+            Image image1 = imgA.Image;
+
+            if (image1 == null)
+            {
+                MessageBox.Show("Por favor, selecione uma imagem no campo Imagem A");
+                return;
+                
+            }
+
+            Bitmap imagemResultado = new Bitmap(image1.Width * 2, image1.Height);
+
+            using (Graphics g = Graphics.FromImage(imagemResultado))
+            {
+                g.DrawImage(image1, 0, 0);
+            }
+
+            Bitmap mirroredImage = new Bitmap(image1);
+            mirroredImage.RotateFlip(RotateFlipType.RotateNoneFlipX);
+
+            using (Graphics g = Graphics.FromImage(imagemResultado))
+            {
+                g.DrawImage(mirroredImage, image1.Width, 0);
+            }
+
+            imgFinal.Image = imagemResultado;
+        }
+
+        private void btExer7_Click(object sender, EventArgs e)
+        {
+            int[,] matriz1;
+            matriz1 = new int[3, 3];
+
+            int[,] matriz2;
+            matriz2 = new int[3, 3];
+
+            int[,] matrizAux;
+            matrizAux = new int[3, 3];
+
+            int[,] saida1;
+            saida1 = new int[3, 3];
+
+            int[,] saida2;
+            saida2 = new int[3, 3];
+
+            Random randNum = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int num;
+
+                    num = randNum.Next(0, 255);
+
+                    matriz1[i, j] = num;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int pixel;
+
+                    pixel = randNum.Next(0, 255);
+
+                    matriz2[i, j] = pixel;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int num;
+
+                    if (matriz1[i, j] + matriz2[i, j] > 255) num = 255;
+                    else num = matriz1[i, j] + matriz2[i, j];
+
+                    saida1[i, j] = num;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int num;
+
+                    num = matriz1[i, j] + matriz2[i, j];
+
+                    matrizAux[i, j] = num;
+                }
+            }
+
+
+            int pxMax = 0;
+            int pxMin = 255;
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int num = matriz1[i, j] + matriz2[i, j];
+
+                    if (num > pxMax) pxMax = num;
+                    if (num < pxMin) pxMin = num;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int pixel_normalizado;
+
+                    pixel_normalizado = (int)(((double)(matrizAux[i, j] - pxMin) / (pxMax - pxMin)) * 255);
+
+                    saida2[i, j] = pixel_normalizado;
+                }
+            }
+
+            ExibirMatriz(matriz1, label1);
+            ExibirMatriz(matriz2, label2);
+            ExibirMatriz(matrizAux, label3);
+            ExibirMatriz(saida1, label4);
+            ExibirMatriz(saida2, label5);
+
+        }
+
+        private void ExibirMatriz(int[,] matriz, Label label)
+        {
+            string texto = "";
+            int largura = matriz.GetLength(0);
+            int altura = matriz.GetLength(1);
+
+            for (int i = 0; i < largura; i++)
+            {
+                for (int j = 0; j < altura; j++)
+                {
+                    texto += matriz[i, j] + " ";
+                }
+                texto += "\n";
+            }
+
+            label.Text = texto;
         }
     }
 }
